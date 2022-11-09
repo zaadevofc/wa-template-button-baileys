9a2325b56e9e2f38b06fa4c0fd4860751b45680f
zaadevofc <93970726+zaadevofc@users.noreply.github.com>
2022-11-09 18:45:25 +0700

    add : copy button

---
+++ button.js
@@ -0,0 +1,55 @@
+const URL = [{
+
+    index: Number,
+
+    urlButton: {
+
+        displayText: String,
+
+        url: 'URL'
+
+    }
+
+}]
+
+const Reply = [{
+
+    index: Number,
+
+    quickReplyButton: {
+
+        displayText: String,
+
+        id: Number
+
+    }
+
+}]
+
+const Copy = [{
+
+    index: Number,
+
+    urlButton: {
+
+        displayText: String,
+
+        url: 'https://www.whatsapp.com/otp/copy/' + 'teks yg mau di salin'
+
+    }
+
+}]
+
+const Phone = [{
+
+    index: Number,
+
+    callButton: {
+
+        displayText: String,
+
+        phoneNumber: Number
+
+    }
+
+}]
